e835b780096bb8a999c3c4f07ce26a8d7eb45428
Unuafe Ajiri <aj4short@rocketmail.com>
2024-12-24 04:07:46 +0100

    create a gameBoard function
    The function should track the state or current status of the gameboard

---
+++ script.js
@@ -0,0 +1,23 @@
+/* create a function that keeps track of the state of the
+ state of the board
+
+ The idea of this function helps you to keep track and
+monitor everything happening on the board
+
+As you can see in the below Gameboard function, we have
+access to each cell and know exactly where they are
+on the gameboard
+ */
+function Gameboard(){
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+    for(let i = 0; i < rows; i++){
+        board[i] = [];
+        for(let j = 0; j < columns; j++){
+            board.push('null')
+        }
+    }
+    console.log(board)
+}
+Gameboard()
